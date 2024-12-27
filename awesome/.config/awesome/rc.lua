2de54c76e1f4d1d7dbb915ac5580a58ea8472ba6
tengng <nguyentin148@gmail.com>
2024-12-28 00:05:41 +0700

    feat: updates + ghostty config

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -2,8 +2,6 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
--- local volumn = require("volume")
-
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
@@ -52,15 +50,16 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
--- beautiful.useless_gap = 7
+beautiful.useless_gap = 10
 -- beautiful.font = "IosevkaTerm Nerd Font 10"
 
--- -- Custom Widgets
--- local battery_widget = require("dtien.battery")
--- local disk_widget = require("dtien.disk")
+-- Custom Widgets
+local battery_widget = require("dtien.widgets.battery")
+local disk_widget = require("dtien.widgets.disk")
+local mem_widget = require("dtien.widgets.mem")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
+terminal = "ghostty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -184,7 +183,7 @@ local function set_wallpaper(s)
             wallpaper = wallpaper(s)
         end
 
-        local filename = "~/.config/awesome/wallpaper.png"
+        local filename = os.getenv "HOME" .. "/personal/wallpapers/main.png"
         local f = io.open(filename, "r")
         if f ~= nil then
             gears.wallpaper.maximized(filename, s, true)
@@ -247,6 +246,9 @@ awful.screen.connect_for_each_screen(function(s)
 
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            mem_widget,
+            disk_widget,
+            battery_widget {},
             mytextclock,
             mykeyboardlayout,
             wibox.widget.systray(),
@@ -393,6 +395,10 @@ clientkeys = gears.table.join(
     awful.key({ modkey,           }, "f",
         function (c)
             c.fullscreen = not c.fullscreen
+            -- disable floating when in fullscreen
+            if c.fullscreen then
+                c.floating = false
+            end
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
@@ -651,7 +657,7 @@ client.connect_signal("unfocus", function(c)
 end)
 -- }}}
 
-awful.util.spawn_with_shell("nitrogen --restore")
+-- awful.util.spawn_with_shell("nitrogen --restore")
 awful.util.spawn_with_shell("~/.local/bin/startup")
 awful.util.spawn("firefox", { tag = "2" })
-awful.util.spawn("gnome-terminal", { tag = "3" })
+awful.util.spawn(terminal, { tag = "3" })
